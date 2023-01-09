6e3fdab68751864f464190fc6882574698b54737
noh06 <noh06@LAPTOP-BU12FKTE>
2023-01-09 20:29:37 +0900

    230109 2030

--- Ch2/src/Prac2.java
+++ Ch2/src/Prac2.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Prac2 {
 	public static void main(String[] args) {
 	    long regNo = 9806132000000L;//리터럴이라서 접미사L 필요
@@ -7,3 +6,4 @@ public class Prac2 {
 	    System.out.println(regNo);
 	}
 }
+
