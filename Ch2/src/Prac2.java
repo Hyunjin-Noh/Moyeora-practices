8fa3f2b16d40c0b1d34656bc3f08336caa89806c
noh06 <noh06@LAPTOP-BU12FKTE>
2023-01-09 20:01:23 +0900

    230109 20

---
+++ Ch2/src/Prac2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Prac2 {
+	public static void main(String[] args) {
+	    long regNo = 9806132000000L;//리터럴이라서 접미사L 필요
+	    /*확인코드*/
+	    System.out.println(regNo);
+	}
+}
